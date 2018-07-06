cdefcd15133c66e49b725fb71ca5ab501eabd4ad
ValokAsura <40879862+ValokAsura@users.noreply.github.com>
2018-07-06 00:19:48 -0500

    Trade helper for FFXI
    Allows you to quickly trade crystals to the Ephemeral Moogles. Requires TradeNPC addon.

---
+++ addons/CrystalTrader/CrystalTrader.lua
@@ -0,0 +1,99 @@
+--[[
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org/>
+]]
+
+_addon.name = 'Crystal Trader'
+_addon.author = 'Valok@Asura'
+_addon.version = '1.0.0.0'
+_addon.command = 'ctr'
+
+exampleOnly = false
+
+windower.register_event('addon command', function(...)
+	local crystalIDs = {
+		{4096, 'fire crystal', 0},
+		{4097, 'ice crystal', 0},
+		{4098, 'wind crystal', 0},
+		{4099, 'earth crystal', 0},
+		{4100, 'lightning crystal', 0},
+		{4101, 'water crystal', 0},
+		{4102, 'light crystal', 0},
+		{4103, 'dark crystal', 0},
+		{4104, 'fire cluster', 0},
+		{4105, 'ice cluster', 0},
+		{4106, 'wind cluster', 0},
+		{4107, 'earth cluster', 0},
+		{4108, 'lightning cluster', 0},
+		{4109, 'water cluster', 0},
+		{4110, 'light cluster', 0},
+		{4111, 'dark cluster', 0},
+	}
+	
+	local inventory = windower.ffxi.get_items(0)
+	local numTrades = 0 
+	
+	for i = 1, 16 do
+		for k, v in ipairs(inventory) do
+			if v.id == crystalIDs[i][1] then
+				crystalIDs[i][3] = crystalIDs[i][3] + v.count
+			end
+		end
+	end
+	
+	for i = 1, 8 do
+		if crystalIDs[i][3] > 0 or crystalIDs[i + 8][3] > 0 then
+			numTrades = numTrades + 1
+		end
+	end
+	
+	if numTrades > 0 then
+		local tradeString = ''
+		
+		for i = 1, 8 do
+			tradeString = '//tradenpc '
+			
+			if crystalIDs[i][3] > 0 then
+				tradeString = tradeString..crystalIDs[i][3]..' "'..crystalIDs[i][2]..'"'
+			end
+			
+			if crystalIDs[i + 8][3] > 0 then
+				tradeString = tradeString..' '..crystalIDs[i + 8][3]..' "'..crystalIDs[i + 8][2]..'"'
+			end
+			
+			if tradeString ~= '//tradenpc ' then
+				if exampleOnly then
+					print(tradeString)
+				else
+					windower.send_command('input '..tradeString)
+					windower.add_to_chat(8, (numTrades - 1)..' trades remaining')
+					break
+				end
+			end
+		end
+	else
+		windower.add_to_chat(8, "Crystal Trader - No crystals in inventory")
+	end
+end)
---
+++ addons/CrystalTrader/readme.md
@@ -0,0 +1,59 @@
+IMPORTANT
+---------
+
+This addon requires the addon "TradeNPC" to be installed and loaded before use.
+TradeNPC can be found here: https://github.com/Ivaar/Windower-addons
+
+
+What does this addon do?
+------------------------
+
+Crystal Trader searches your inventory for standard elemental crystals and clusters, then prepares a command to be used by TradeNPC.
+This allows you to automatically trade stacks of similar clusters and crystals to the Ephemeral Moogles in the crafting guilds.
+
+
+Example
+-------
+
+You have 3 stacks of Fire Crystals, 1 stack of Ice Crystals, and 2 Fire Clusters in your inventory that you would like to store.
+Travel to and target one of the Ephemeral Moogles you would like to trade to.
+In the chat window, enter '//ctr' without the quotes and press enter, or type 'ctr' in the console
+The addon will cause TradeNPC to instantly trade all 3 stacks of Fire Crystals and the 2 Fire Clusters with one command.
+After you wait for the moogle to do his thing, target the moogle again and re-enter the command.
+The addon will then cause TradeNPC to instantly trade the stack of Ice Crystals.
+
+You will need to enter the command once for each crystal element you possess in your inventory.
+
+If you wish to test this addon before using it, you may edit line 33 in the CrystalTrader.lua file.
+
+Change
+	exampleOnly = false
+to
+	exampleOnly = true
+	
+This will cause the addon to print the commands that will be issued to the windower console.
+When you are ready to allow the addon to enter commands for you, change the 'true' back to 'false' and visit an Ephemeral Moogle.
+
+
+Installation
+------------
+
+Browse to your Windower\addons folder and create a new folder inside called "CrystalTrader"
+Place CrystalTrader.lua in this new folder.
+Load the addon by accessing the console from within FFXI and typing 'lua l CrystalTrader'
+
+If you want this addon to be loaded automatically every time you launch the game,
+add 'lua l CrystalTrader' to the bottom of the file Windower\scripts\init.txt.
+
+I would also recommend having the addon TradeNPC load automatically by adding
+'lua l TradeNPC' to your init.txt.
+
+
+Additional Information and Disclaimer
+-------------------------------------
+
+Crystal Trader only scans your inventory for the elemental crystals and clusters then enters a command that will be used by the TradeNPC addon.
+No other actions are carried out. This addon will not function if you do not have the TradeNPC addon loaded and working.
+
+I am an amateur programmer and have never tried anything with LUA until now. Please feel free to modify/change/steal any of the code.
+If you end up making this addon better, please let me know!
